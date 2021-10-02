c405aeee97e5fb3642d35b28cfc14734bf331c22
Juan Ricci <juanrricci@gmail.com>
2021-10-02 13:25:52 -0300

    Added and populated Faq section file, imported in index.js

---
+++ src/pages/landing-page-sections/Faq.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default function Faq() {
+    return <>
+    <section id="faq" className="faq">
+        <div className="container">
+            <div className="row text-center">
+                <h1 className="display-3 fw-bold">Preguntas frecuentes</h1>
+                <div className="heading-line mb-5"></div>
+            </div>
+        </div>
+    </section>
+    </>
+}
