7bd2cee65e191b735b65aa18f4011e3ed73c0a33
Juan Ricci <juanrricci@gmail.com>
2021-10-03 12:04:04 -0300

    Reverted Masonry layout module import

--- src/pages/conversations.js
+++ src/pages/conversations.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import Masonry from 'masonry-layout';
 
+// Masonry layout will be imported soon...
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 export default function ConversationsPage() {
-    var msnry = new Masonry( '.grid');
     return <>
     <Navbar />
-    <h1>This is the Conversations page boilerplate</h1>
-    <p>Soon My Son, please don't desperate</p>
     <div class="row" data-masonry='{"percentPosition": true }'>
     <div class="col-sm-6 col-lg-4 mb-4">
       <div class="card">
