791c75845b2f8668f3670192df9015db0720be0e
Juan Ricci <juanrricci@gmail.com>
2021-10-02 21:57:32 -0300

    Added masonry-layout module and imported in conversations page

--- src/pages/conversations.js
+++ src/pages/conversations.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Masonry from 'masonry-layout';
 
 import Navbar from "../components/Navbar";
 
 export default function ConversationsPage() {
+    var msnry = new Masonry( '.grid');
     return <>
     <Navbar />
     <h1>This is the Conversations page boilerplate</h1>
