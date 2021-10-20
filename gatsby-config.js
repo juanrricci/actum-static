6277f41a55dce12b6e1f56946dcaaa9ee4142575
Juan Ricci <juanrricci@gmail.com>
2021-10-20 14:27:44 -0300

    Added wpgraphql settings for fetching posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,22 @@ module.exports = {
     title: "Actum Full Project",
   },
   plugins: [
+        /*
+     * Gatsby's data processing layer begins with “source”
+     * plugins. Here the site sources its data from WordPress.
+     */
+    // highlight-start
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        /*
+         * The full URL of the WordPress site's GraphQL API.
+         * Example : 'https://www.example-site.com/graphql'
+         */
+        url: `http://localhost:8888/index.php?graphql`,
+      },
+    },
+    // highlight-end
     "gatsby-plugin-sass",
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
